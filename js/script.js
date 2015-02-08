13257bfe26590fa0588c56f806bac1e8dc0c26d5
Luke <tech.master.champ@gmail.com>
2015-02-08 08:57:07 -0700

    Beautified JavaScript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-$(function(){var a=$("form"),b=$('input[type="text"]'),c=$(".error"),d=$("ul"),e=$("#new"),f=$("print"),g=$("#save"),h=$("#load"),i=[];f.click(function(){a.hide(),window.print(),a.show()}),a.submit(function(a){a.preventDefault();var e=b.val();""!=e&&null!=e?(i.push(e),d.append($("<li/>").html(e)),b.val(""),c.empty()):c.html("Please enter a list item into the input box")}),e.click(function(){i=[],d.empty()}),g.click(function(){localStorage.setItem("items",JSON.stringify(i))}),h.click(function(){var a=localStorage.getItem("items");console.log(a),JSON.parse(a).forEach(function(a){i.push(a),d.append($("<li/>").html(a))})})});
+$(function() {
+    var a = $("form"),
+        b = $('input[type="text"]'),
+        c = $(".error"),
+        d = $("ul"),
+        e = $("#new"),
+        f = $("print"),
+        g = $("#save"),
+        h = $("#load"),
+        i = [];
+    f.click(function() {
+        a.hide(), window.print(), a.show()
+    }), a.submit(function(a) {
+        a.preventDefault();
+        var e = b.val();
+        "" != e && null != e ? (i.push(e), d.append($("<li/>").html(e)), b.val(""), c.empty()) : c.html("Please enter a list item into the input box")
+    }), e.click(function() {
+        i = [], d.empty()
+    }), g.click(function() {
+        localStorage.setItem("items", JSON.stringify(i))
+    }), h.click(function() {
+        var a = localStorage.getItem("items");
+        console.log(a), JSON.parse(a).forEach(function(a) {
+            i.push(a), d.append($("<li/>").html(a))
+        })
+    })
+});
